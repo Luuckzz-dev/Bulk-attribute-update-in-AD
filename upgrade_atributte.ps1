b5f5a114e7f835e98e611fdf3b06bcc6a7e468ed
Lucas Gonzales <63990673+Luuckzz-dev@users.noreply.github.com>
2023-10-29 15:56:54 -0300

    Add files via upload

---
+++ upgrade_atributte.ps1
@@ -0,0 +1,31 @@
+﻿# Caminho para o arquivo Excel
+$excelFilePath = "C:\Users\lgonzales\Desktop\shellscript\carlos\app\testead.xlsx"
+
+# Nome da coluna que contém os nomes de usuários no Excel
+$usuarioColumnName = "Usuários"
+
+# Nome da coluna que contém as informações para atualizar no AD
+$sobrenomeCargoColumnName = "Sobrenome do Cargo"
+
+# Importar o módulo do Active Directory
+Import-Module ActiveDirectory
+
+# Ler o arquivo Excel e extrair os dados
+$excelData = Import-Excel -Path $excelFilePath
+
+# Iterar pelos dados do Excel
+foreach ($row in $excelData) {
+    $usuario = $row.$usuarioColumnName
+    $sobrenomeCargo = $row.$sobrenomeCargoColumnName
+
+    # Buscar o usuário no Active Directory
+    $user = Get-ADUser -Filter {SamAccountName -eq $usuario}
+
+    if ($user) {
+        # Atualizar o atributo "comment" no Active Directory
+        Set-ADUser -Identity $user -Replace @{comment = $sobrenomeCargo}
+        Write-Host "Usuário $usuario encontrado no AD e atributo 'comment' atualizado."
+    } else {
+        Write-Host "Usuário $usuario não encontrado no AD."
+    }
+}
